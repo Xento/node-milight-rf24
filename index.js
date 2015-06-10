0e1e45d5f694d17550d97220bff2ca5ad57487ad
Markus <markus-mi@web.de>
2015-06-10 13:06:21 +0200

    Added functions to translate milightcolor to hsv and rgb

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ var MilightRF24Controller = function (options) {
 	});
 
 	this._emitter.on("dataReceived", function (data){
+		var self = this;
+		
 		data = String(data).split(" ");
 		console.log("data:"+data);
 		var id = ""+data[1]+data[2];
@@ -94,7 +96,7 @@ var MilightRF24Controller = function (options) {
 		}
 		else
 		
-		console.log(cmd);
+		color = self._MilightColorToRGB(color);
 		
 		var dataObj = {
 			raw: data,
@@ -252,6 +254,93 @@ MilightRF24Controller.prototype._hsvToMilightColor = function (hsv){
     var color = (256 + 26 - Math.floor(Number(hsv[0]) / 360.0 * 255.0)) % 256;
     return color;
 };
+
+/**
+ * HSV to RGB color conversion
+ *
+ * H runs from 0 to 360 degrees
+ * S and V run from 0 to 100
+ * 
+ * Ported from the excellent java algorithm by Eugene Vishnevsky at:
+ * http://www.cs.rit.edu/~ncs/color/t_convert.html
+ */
+MilightRF24Controller.prototype._hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
+
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
+
+    if (s == 0) {
+        // Achromatic (grey)
+        r = g = b = v;
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+
+    switch (i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+
+        default:
+            // case 5:
+            r = v;
+            g = p;
+            b = q;
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+MilightRF24Controller.prototype._MilightColorToRGB(milicolor) {
+	var c1 = (Math.floor((milicolor / 255.0 * 359.0) % 360) - 240);
+	var color = c1 <= 0 ? Math.abs(c1) : 360 - c1;
+	rgb = hsvToRgb(color, 80, 100).join();
+	return rgb;
+});
 
 var RGBWButtons = function(){};
 RGBWButtons.prototype.AllOn = 0x01;
